feeca9f986b880714e432a8d4d61891a138a0782
luonginfosec <luonginfosec@gmail.com>
2024-10-31 23:27:19 +0700

    binToHex

---
+++ bintohex.py
@@ -0,0 +1,13 @@
+# Read binary data from jpg2bin.txt
+with open('', 'r') as file:
+    binary_string = file.read().strip()
+
+binary_segments = binary_string.split()
+
+hex_segments = [hex(int(segment, 2))[2:].zfill(2).upper() for segment in binary_segments]
+
+hex_string = " ".join(hex_segments)
+
+with open('', 'w') as file:
+    file.write(hex_string)
+
